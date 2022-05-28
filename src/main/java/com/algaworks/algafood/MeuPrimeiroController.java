cf017585ba9d697d1b3d478977ba9ddb66862835
leoarcabold <leoarcabold@gmail.com>
2022-05-28 20:48:49 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
+@Controller
+public class MeuPrimeiroController {
+	private AtivacaoClienteService ativacaoClienteService;
+
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+		System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		Cliente joao = new Cliente("João","joao@gmail.com","11 99999-9999");
+		ativacaoClienteService.ativar(joao);
+		return joao.getNome()+" "+joao.getEmail()+" "+joao.getTelefone();
+				
+		//return "Hello";
+	}
+
+}
